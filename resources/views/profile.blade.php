b62abe6de6c5d703db92a9bc2560e0239e5c007e
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2024-09-06 21:03:39 +0700

    merge

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -61,22 +61,6 @@
                     <p class="font-poppins text-base md:text-xl font-medium text-white">Transaction</p>
                 </a>
             </div>
-            <div class="w-full px-8 md:px-12 mb-4">
-                <a href="{{ route('account') }}"
-                    class="w-full bg-red-500 flex items-center px-5 hover:scale-105 duration-300 transition-all rounded-xl space-x-4 md:space-x-5 h-12 md:h-16">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="ionicon w-8 md:w-12 h-8 md:h-12"
-                        viewBox="0 0 512 512">
-                        <path d="M256 80a176 176 0 10176 176A176 176 0 00256 80z" fill="none" stroke="#fff"
-                            stroke-miterlimit="10" stroke-width="32" />
-                        <path
-                            d="M200 202.29s.84-17.5 19.57-32.57C230.68 160.77 244 158.18 256 158c10.93-.14 20.69 1.67 26.53 4.45 10 4.76 29.47 16.38 29.47 41.09 0 26-17 37.81-36.37 50.8S251 281.43 251 296"
-                            fill="none" stroke="#fff" stroke-linecap="round" stroke-miterlimit="10"
-                            stroke-width="28" />
-                        <circle cx="250" cy="348" r="20" stroke="#fff" fill="#fff" />
-                    </svg>
-                    <p class="font-poppins text-base md:text-xl font-medium text-white">Information</p>
-                </a>
-            </div>
             <div class="w-full px-8 md:px-12 mt-7">
                 <a href="#"
                     class="w-full bg-red-800 flex px-5 items-center hover:scale-105 duration-300 transition-all rounded-xl space-x-4 md:space-x-5 h-12 md:h-16">
